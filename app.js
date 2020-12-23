382a8ff5cb0ea9e170c2c986a45acf93c31efc0d
ZION <sju0924@khu.ac.kr>
2020-12-23 18:26:43 +0900

    Add repository merona-web
    add app.js
    link heroku

---
+++ app.js
@@ -0,0 +1,18 @@
+const https = require('http');
+var express = require('express');
+
+const server = https.createServer((request,response) => { 
+    response.writeHead(200, { 'Content-Type' : 'text/html'} );
+    if(request.url === '/'){
+        response.write("Hello, it's Merona web development project");
+    }
+    else{
+        var resString = String(request.url);
+        response.write(resString);
+    }
+    response.end();
+    console.log(request); 
+}); 
+
+server.listen(3000);
+
