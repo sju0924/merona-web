1f693beaba9d3134f9eb168cbc053e8802817086
ZION <sju0924@khu.ac.kr>
2020-12-30 21:48:32 +0900

    Add posts list
    Getting data from MongoDB, Service shows that data to user.

--- app.js
+++ app.js
@@ -8,56 +8,80 @@ mongoose.connect(uri,{useNewUrlParser: true ,useUnifiedTopology: true ,});
 var db = mongoose.connection;
 var currentindex = 0;
 
+//게시판 게시물의 형식
 var BoardSchema = mongoose.Schema({
     Title: String,
     Author: String,
     Content: String,
     Index: String
 });
+//boardmodels collection을 연결
+var BoardModel = mongoose.model("boardmodels", BoardSchema);
 
-
-
-app.use(express.static(__dirname + '/html/resources'));
+//메인 화면(index) 로 route
+app.use(express.static(__dirname + '/resources'));
 app.get('/',function(req,res){
-    res.sendFile(__dirname + '/html/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
-
-app.use('/board',express.static(__dirname + '/html/resources'));
+// 게시판으로 route
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 app.get('/board',function(req,res){
-    res.sendFile(__dirname + '/html/board.html');
+    
+    BoardModel.find(function(err,data){ // 게시물을 MongoDB에서 가져오기
+        if(err) res.send(err);
+        console.log(data);
+        res.render("board.html",{"content":data}); // merona-web\views에 board.html이 있음.
+    });
+    
+  //res.send("12345");
+    
 })
 
+//게시판-글쓰기로 route
+app.get('/board/write',function(req,res){
+    res.sendFile(__dirname + '/views/write.html');
+})
+
+//게시판-제출로 write. 사용자가 직접 사용하는 페이지는 아님.
 app.get('/submit',function(req,res){
+    //Query 저장
     var gettitle = req.query.title;
     var getauthor = req.query.author;
     var getcontent = req.query.content;
-    console.log(req);
+    //console.log(req);
 
-    var BoardModel = mongoose.model("BoardModel", BoardSchema);
+
+    //데이터 생성
     var Ins = new BoardModel({ 
 		Title: gettitle,
 		Author: getauthor,
 		Content: getcontent,
-		Index : toString(currentindex)		
+		Index : currentindex		
     });
-   console.log(Ins.Title); // "testIns"
+   console.log(Ins.Title); 
     
 
-/*
+    //MongoDB에 데이터 저장
 	Ins.save(function(err, testIns){
-		if(err) return console.error(err);
-        
+		if(err) return console.error(err);        
     });
     
+    //다음 게시글 번호
     currentindex = currentindex+1;
-    */
+    
+    // 글쓰기 submit시 redirect되어서 MongoDB에 저장 후 게시판으로 redirect
+    res.redirect('/board'); 
 })
 
+
+//to-do 로 route
 app.get('/list',function(req,res){
-    res.sendFile(__dirname + '/html/list.html');
+    res.sendFile(__dirname + '/views/list.html');
 })
 
+//포트 5000에서 앱 실행
 app.listen(process.env.PORT || 5000,function(){
     console.log("Server has created");   
 })
