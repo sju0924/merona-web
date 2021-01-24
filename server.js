10da44fefa46eeaabf7b17d4cd3928e9aaac12b7
ZION <sju0924@khu.ac.kr>
2021-01-24 19:49:18 +0900

    ㅗ
    ㅅㅂ

--- server.js
+++ server.js
@@ -107,11 +107,10 @@ app.get('/submit',function(req,res){
 })
 
 // 리액트 정적 파일 제공
-//app.use(express.static(__dirname +'/todolist_client/build'));
+app.use(express.static(__dirname +'/todolist_client/build'));
 //to-do 로 route
 app.get('/list',function(req,res){
-    //res.sendFile(__dirname+'/todolist_client/build/index.html');
-    res.send(1);
+    res.sendFile(__dirname+'/todolist_client/build/index.html');
 })
 
 app.get('/api/data',function(req,res){
